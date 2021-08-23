d883ce21d445bc2f3b3f97ed30fb05ee172c528c
Dan Wu <developerdanwu@gmail.com>
2021-08-24 01:08:40 +0800

    fix testimonials

--- components/TestimonialItem.js
+++ components/TestimonialItem.js
@@ -2,10 +2,10 @@ import {Link,Button,Image, Heading, Text, Flex, VStack, Box, Spacer, Center } fr
 
 function TestimonialItem(props) {
     return (
-        <Text lineHeight="28px" border="2px" borderColor="brand.main" padding="13px">{props.content}<br/><br/>
+        <Box lineHeight="28px" border="2px" borderColor="brand.main" padding="13px" mb="15px">{props.content}<br/><br/>
 <Flex alignItems="center" justifyContent="space-between"><Text fontWeight="600">--{props.client}</Text>{props.link ? <Button _hover={{backgroundColor:"brand.hover", color:"white", borderColor:"brand.hover"}} borderColor="brand.main" borderWidth="2px" padding="5px">
         <Link _hover={{textDecoration:"none"}} href={props.link} fontWeight="black" fontSize="sm">Link to work</Link>
-        </Button>:null}</Flex></Text>
+        </Button>:null}</Flex></Box>
     )
 }
 
