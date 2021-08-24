903226c178d5ceba8c59efb224e9e1fa717b4086
Dan Wu <developerdanwu@gmail.com>
2021-08-25 00:39:24 +0800

    minor

--- components/Layout.js
+++ components/Layout.js
@@ -52,10 +52,9 @@ function useWindowSize() {
     return (
         <Flex alignItems={{base:"flex-start",sm:"center"}} justifyContent={{base:"flex-start",sm:"center"}} height={{base:"100%", sm:"100vh"}} flexDir="column">
 
-        <Flex height={{base:"100vh", sm:"100vh", md:"100vh", lg:"100vh"}} justifyContent="space-around" alignItems="center" flexDir="column" px={{base:"13px", sm:"20px", md:"20px", lg:"20px"}}  justify="center" width="100%" maxW="1250px" maxH={{base:"",sm:"1000px"}}>
+        <Flex height={{base:"100%", sm:"100vh", md:"100vh", lg:"100vh"}} justifyContent="space-around" alignItems="center" flexDir="column" px={{base:"13px", sm:"20px", md:"20px", lg:"20px"}}  justify="center" width="100%" maxW="1250px" maxH={{base:"",sm:"1000px"}}>
         <Nav/>
         {size <540 ? null :<Spacer/>}
-        {size < 640 ? <Box pt="20px" pb="120px" height="100%">{children}</Box> : null}
 
         {size >= 640 ? <Box ml="20px" mr="20px"
         overflowY="hidden"
@@ -88,8 +87,7 @@ function useWindowSize() {
                 </HStack>
             </Box>
             {children}
-
-        </Box>: null}
+        </Box>: <Box pt="20px" pb="120px" height="100%">{children}</Box>}
         <Spacer/>
         <Footer/>
         </Flex>
